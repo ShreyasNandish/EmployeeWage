3dfe7478a676f1091ebdeaf5c51edcf417f4695d
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-07 23:00:13 +0530

    Modified the EmployeeWage to calculate the sallary if present for a day

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,12 +1,18 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int IS_PRESENT=1;
+    static final int WAGE_PER_HOUR=20;
+    static final int FULL_DAY_HOUR=8;
     public static void main(String[] args) {
         System.out.println("Welcome to employee program");
-        int attendence=(int)Math.floor((Math.random()*10))%2;
+        int attendence=(int)(Math.floor(Math.random()*10))%2;
 
-        if(attendence==1)
+        if(attendence==IS_PRESENT) {
             System.out.println("Employee is present");
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee wage for a day is:" + dailyWage);
+        }
         else
             System.out.println("Employee is absent");
     }
