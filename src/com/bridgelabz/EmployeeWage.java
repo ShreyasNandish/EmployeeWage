5c9224dd85d9e68a9c8c291376bec1da8dd2ca6c
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-08 12:41:40 +0530

    Modifed-Added employeeWage using switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -7,20 +7,27 @@ public class EmployeeWage {
     static final int PART_DAY_HOUR = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static int day = 0;
+    static final int WORKING_DAY_PER_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee program");
-        int empRandom = (int) (Math.floor(Math.random() * 10)) % 3;
+        int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
 
-        if (empRandom == IS_FULL_TIME) {
-            System.out.println("Employee is present Full time ");
-            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Employee wage for a day is:" + dailyWage);
-        } else if (empRandom == IS_PART_TIME) {
-            System.out.println("Employee is present part time");
-            int dailyWage = WAGE_PER_HOUR * PART_DAY_HOUR;
-            System.out.println("Employee wage for a day is:" + dailyWage);
-        } else
-            System.out.println("Employee is Absent ");
+        switch (attendance) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is present Full time ");
+                int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employee wage for a day is:" + dailyWage);
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is present Part time ");
+                dailyWage = WAGE_PER_HOUR * PART_DAY_HOUR;
+                System.out.println("Employee wage for a day is:" + dailyWage);
+                break;
+            default:
+                System.out.println("Employee is absent");
+
+        }
     }
 }
